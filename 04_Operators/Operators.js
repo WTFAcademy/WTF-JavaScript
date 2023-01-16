345ed4fec88609fce2a659714bc1e59826b0a11f
Li, Amazing Ang <d0u.gie@hotmail.com>
2023-01-16 17:33:06 +0800

    update 04 operators

--- 04_Operators/Operators.js
+++ 04_Operators/Operators.js
@@ -1,6 +1,35 @@
-console.log(1 + (2 ** 3) / 2)
+// 算数运算符
+let num1 = 1 + 1; // 2
+let num2 = 1 - 1; // 0
+let num3 = 2 * 3; // 6
+let num4 = 6 / 2; // 3
+let num5 = 7 % 2; // 1
+let num6 = 2 ** 3; // 8
+num1++ // 3
+num2--; // -1
 
-let x = 10
-console.log(x++ % 2, x)
+// 比较运算符
+let x = 1;
+let y = 2;
+let bool1 = (x == y); // false
+let bool2 = (x != y); // true
+let bool3 = (x > y); // false
+let bool4 = (x < y); // true
+let bool5 = (x >= y); // flase
+let bool6 = (x <>= y); // true
+let bool7 = (x === y); // flase
+let bool8 = (x !== y); // true
 
-console.log(null || 1 && 2 || 3)
+// 逻辑运算符
+true || true; // true
+false || true; // true
+true || false; // true
+false || false; // false
+
+true && true; // true
+false && true; // false
+true && false; // false
+false && false; // false
+
+!true; // false
+!false; // true
