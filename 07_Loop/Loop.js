81a878c072a5151ad8247a3501ed54391124a0fa
Li, Amazing Ang <d0u.gie@hotmail.com>
2023-02-12 22:19:24 +0700

    update 07 loop

--- 07_Loop/Loop.js
+++ 07_Loop/Loop.js
@@ -1,93 +1,46 @@
 /** while */
 {
-  let i = 0
+  console.log("for循环");
+  for (let i = 0; i < 10; i++) {
+    console.log(`i 当前的值为：${i}`);
+  }
+}
+// i 当前的值为：0
+// i 当前的值为：1
+// ...
+// i 当前的值为：9
 
-  while (i < 3) {
+/** while */
+{
+  console.log("while循环");
+  let i = 0
+  while (i < 10) {
     console.log(`i 当前的值为：${i}`)
     i++
-  }
-
-  // i 当前的值为：0
-  // i 当前的值为：1
-  // i 当前的值为：2
+  }  
 }
 
-/** do while */
+/** do-while */
 {
+  console.log("do-while循环");
   let i = 0
-
   do {
-    console.log(`i 当前的值为：${i}`)
-    i++
-  } while (i < 3)
-
-  // i 当前的值为：0
-  // i 当前的值为：1
-  // i 当前的值为：2
+    console.log(`i 当前的值为：${i}`);
+    i++;
+  } while (i < 9)
 }
-
-/** for */
-{
-  for (let i = 0; i < 3; i++) {
-    console.log(`i 当前的值为：${i}`)
-  }
-
-  // i 当前的值为：0
-  // i 当前的值为：1
-  // i 当前的值为：2
-}
-
 /** break */
 {
-  let i = 0
-
-  while (i < 3) {
-    console.log(`i 当前的值为：${i}`)
-    i++
-    if (i === 2) break
-  }
-
-  // i 当前的值为：0
-  // i 当前的值为：1
-
   for (let i = 0; i < 3; i++) {
-    if (i === 2) break
-    console.log(`i 当前的值为：${i}`)
+    console.log(`i 当前的值为：${i}`);
+    if (i === 2) break;
   }
-
-  // i 当前的值为：0
-  // i 当前的值为：1
 }
 
 /** continue */
 {
   for (let i = 0; i < 10; i++) {
-    if (i % 2 == 0) continue
-
+    if (i % 2 === 0) continue
     console.log(`i 当前的值为：${i}`)
   }
-
-  // i 当前的值为：1
-  // i 当前的值为：3
-  // i 当前的值为：5
-  // i 当前的值为：7
-  // i 当前的值为：9
-}
-
-/** label */
-{
-  let n = 0
-
-  outPoint: for (let i = 0; i < 10; i++) {
-    // i 循环
-    for (let j = 0; j < 10; j++) {
-      // j 循环
-      if (i === 5 && j === 5) {
-        break outPoint // 在 i = 5，j = 5 时，跳出所有循环，返回到整个 outPoint 下方，继续执行
-      }
-      n++
-    }
   }
-
-  console.log(n) // 55
-}
