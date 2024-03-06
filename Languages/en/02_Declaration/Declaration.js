85fac78c9f4a597ad2fb53d54d11317b7ab936dd
Mosamorphing <121625762+Mosamorphing@users.noreply.github.com>
2024-03-06 11:53:09 +0100

    Create Declaration.js

---
+++ Languages/en/02_Declaration/Declaration.js
@@ -0,0 +1,18 @@
+// let
+// 1. Declare the year variable first, and then assign it the value 2023
+let year;
+year = 2023;
+// 2. Declare the name variable and assign it the value "0xAA"
+let name = "0xAA";
+// 3. Declare the newName variable and assign the value "0xAA" of the variable name to it.
+let newName = name;
+
+// const
+// Declare the age constant and assign it a value of 18
+const age = 18;
+// Attempting to change the value of a constant will result in an error TypeError: Assignment to constant variable
+age = 20;
+
+// var (not recommended)
+// Declare variable variable and assign value "hello"
+var variable = "hello"
